9bb3bc9f9f9ea3ad9e7b72ddbb00202b438bf8e0
anonuserhere <131941537+anonuserhere@users.noreply.github.com>
2023-06-05 16:52:03 +0800

    added q7

--- q7/script.js
+++ q7/script.js
@@ -1,3 +1,18 @@
+const months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+
 const chartOptions = {
   chart: {
     type: "bar",
@@ -23,6 +38,48 @@ async function loadData() {
     };
   });
   console.log(earnings);
+
+  let filtered = earnings.filter(function (xy) {
+    return xy.date.getFullYear() == 2020;
+  });
+  console.log(filtered);
+
+  let byMonth = filtered.map(function (xy) {
+    return {
+      amount: parseInt(xy.amount),
+      month: xy.date.getMonth(),
+    };
+  });
+
+  // reference for groupBy. good to learn
+  var groupBy = function (data, key) {
+    return data.reduce(function (storage, item) {
+      var group = item[key];
+      storage[group] = storage[group] || [];
+      storage[group].push(item);
+      return storage;
+    }, {});
+  };
+  let groups = groupBy(byMonth, "month");
+  console.log(groups);
+
+  // i thought the groupBy was bad enough...
+  let series = Object.values(groups).map(function (group, month) {
+    return {
+      x: months[month],
+      y: group.reduce((a, b) => a + b.amount, 0),
+    };
+  });
+  console.log(series);
+
+  chart.updateSeries([
+    {
+      name: "Sales",
+      data: series,
+    },
+  ]);
 }
 
-loadData();
+window.addEventListener("DOMContentLoaded", async () => {
+  await loadData();
+});
